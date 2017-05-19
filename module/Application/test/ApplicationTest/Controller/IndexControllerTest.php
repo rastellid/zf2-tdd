f0d9859740267247dac552a35b573f24495a4b23
Daniele Rastelli <info@next-technology.eu>
2017-05-19 17:53:26 +0200

    primo commit

---
+++ module/Application/test/ApplicationTest/Controller/IndexControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace ApplicationTest\Controller;
+
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+class IndexControllerTest extends AbstractHttpControllerTestCase
+{
+    protected $traceError = true;
+
+    public function setUp()
+    {
+        $this->setApplicationConfig(
+            include '/Users/Daniele/ProjectTesting/TDD/TestConZF2/config/application.config.php'
+        );
+    }
+
+    public function testIndexActionCanBeAccessed()
+    {
+        $this->dispatch('/');
+        $this->assertResponseStatusCode(200);
+
+        $this->assertModuleName('application');
+        //$this->assertControllerName('application_controller_index');
+        $this->assertTemplateName('application/index/index');
+        $this->assertControllerClass('IndexController');
+        $this->assertQuery('public/css/bootstrap.min.css');
+        $this->assertMatchedRouteName('home');
+    }
+}
